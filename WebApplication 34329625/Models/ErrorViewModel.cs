19c9cb88d1129197c20136d9641e1321bf760f47
megatheron <dupreeztheron@gmail.com>
2022-09-21 13:10:31 +0200

    Add project files.

---
+++ WebApplication 34329625/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication_34329625.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

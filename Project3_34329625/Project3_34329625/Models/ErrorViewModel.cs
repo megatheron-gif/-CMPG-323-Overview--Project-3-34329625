199fcc8f1d62d5bc2f991f56493122d622e21c83
megatheron <dupreeztheron@gmail.com>
2022-09-21 18:20:28 +0200

    ek try

---
+++ Project3_34329625/Project3_34329625/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project3_34329625.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
